5712389d2f1a0b2fc6d00599aa904ce3c6818ce1
Ritraj <c0812652@mylambton.ca>
2021-11-10 16:00:13 -0500

    Adding authentication activity

---
+++ app/src/main/java/com/example/letsvolunteer/Authentication.java
@@ -0,0 +1,26 @@
+package com.example.letsvolunteer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Authentication extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        registerBtn = findViewById(R.id.register_button);
+        loginBtn = findViewById(R.id.login_button);
+
+
+        registerBtn.setOnClickListener(v -> {
+
+        });
+    }
+}
