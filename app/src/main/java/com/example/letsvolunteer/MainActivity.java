ce44c843179f47779e3a4d3f640e938032f23fbd
“Gurpreet_c0823239” <c0823239@mylambton.ca>
2021-11-15 21:14:52 -0500

    loginpage design

--- app/src/main/java/com/example/letsvolunteer/MainActivity.java
+++ app/src/main/java/com/example/letsvolunteer/MainActivity.java
@@ -7,31 +7,19 @@ import android.os.Bundle;
 import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.OnTextChanged;
+
 import java.util.List;
 
 @SuppressLint("NonConstantResourceId")
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.mainRecyclerView)
-    RecyclerView recyclerView;
-    List<MyDataType> list=new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome_page);
         ButterKnife.bind(this);
 
-        LinearLayoutManager layoutManager=new LinearLayoutManager( this);
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
-        MyDataType type=new MyDataType();
-
-
-        type.name=""+type.get("name");
-        type.date_of_post=""+type.get("date_of_post");
-        AdapterProfile adapter2=new AdapterProfile(list);
-        recyclerView.setAdapter(adapter2);
     }
 }
