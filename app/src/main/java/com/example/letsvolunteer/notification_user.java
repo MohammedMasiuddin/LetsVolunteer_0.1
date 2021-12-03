d85045b084c82f69f6c50704632c212368a56651
“Gurpreet_c0823239” <c0823239@mylambton.ca>
2021-12-03 00:54:39 -0500

    working on notification coding

--- app/src/main/java/com/example/letsvolunteer/notification_user.java
+++ app/src/main/java/com/example/letsvolunteer/notification_user.java
@@ -15,49 +15,55 @@ import android.os.Bundle;
 import android.widget.RemoteViews;
 
 public class notification_user extends AppCompatActivity {
-    private static RemoteViews contentView;
-    private static Notification notification;
-    private static NotificationManager notificationManager;
-    private static final int NotificationID = 1005;
-    private static NotificationCompat.Builder mBuilder;
 
-    @SuppressLint("RemoteViewLayout")
-    private void RunNotification() {
 
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mBuilder = new NotificationCompat.Builder(getApplicationContext(), "notify_001");
+    NotificationManager mNotificationManager;
 
-        contentView = new RemoteViews(getPackageName(), R.layout.activity_notification_user);
-        contentView.setImageViewResource(R.id.image, R.mipmap.ic_launcher);
-        Intent switchIntent = new Intent(this,
-                BackgroundService.switchButtonListener.class);
-        PendingIntent pendingSwitchIntent = PendingIntent.getBroadcast(this, 1020, switchIntent, 0);
-      //  contentView.setOnClickPendingIntent(R.id.flashButton, pendingSwitchIntent);
-      //  mBuilder.setSmallIcon(R.mipmap.newicon);
-        mBuilder.setAutoCancel(false);
-        mBuilder.setOngoing(true);
-        mBuilder.setPriority(Notification.PRIORITY_HIGH);
-        mBuilder.setOnlyAlertOnce(true);
-        mBuilder.build().flags = Notification.FLAG_NO_CLEAR | Notification.PRIORITY_HIGH;
-        mBuilder.setContent(contentView);
+    NotificationCompat.Builder mBuilder;
+    NotificationCompat.BigTextStyle bigText;
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId = "channel_id";
-            NotificationChannel channel = new NotificationChannel(channelId, "channel name", NotificationManager.IMPORTANCE_HIGH);
-            channel.enableVibration(true);
-            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-            notificationManager.createNotificationChannel(channel);
-            mBuilder.setChannelId(channelId);
-        }
-
-        notification = mBuilder.build();
-        notificationManager.notify(NotificationID, notification);
-    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_user);
+        showNotification("message passed to notification");
+    }
+    public void showNotification(String name){
+
+        mBuilder= new NotificationCompat.Builder(getApplicationContext(), "notify_001");
+        Intent ii = new Intent(getApplicationContext(),notification_user.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, ii, 0);
+
+        bigText = new NotificationCompat.BigTextStyle();
+        bigText.bigText("verseurl");// download complete
+        bigText.setBigContentTitle(""+name); //image name
+        bigText.setSummaryText("Text in detail");
+
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher_round);
+        mBuilder.setContentTitle("Uploading Image");
+        mBuilder.setProgress(0,0,true);
+        mBuilder.setContentText("Your text");
+        mBuilder.setPriority(Notification.PRIORITY_MAX);
+        mBuilder.setStyle(bigText);
+
+        mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+// === Removed some obsoletes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            String channelId = "Your_channel_id";
+            NotificationChannel channel = new NotificationChannel(
+                    channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            mNotificationManager.createNotificationChannel(channel);
+            mBuilder.setChannelId(channelId);
+        }
+
+        mNotificationManager.notify(0, mBuilder.build());
     }
 }
